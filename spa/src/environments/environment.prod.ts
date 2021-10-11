68a66721760948d7ad9f16b3e49030950dd89435
Tom Bulatewicz <tom@nosleep.net>
2021-10-10 21:57:49 -0700

    Add Angular environment files

---
+++ spa/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://<apim_name>.azure-api.net",
+  websiteUrl: "https://<storage_account_name>.<zone>.web.core.windows.net/",
+  fitbitClientId: "<fitbit_client_id>"
+};
